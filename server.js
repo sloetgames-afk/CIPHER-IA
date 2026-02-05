75b99bbf32f2caa5666ea483d8f06cbf82fe9ed1
Sloet Games™ <sloetgames@gmail.com>
2026-02-05 10:43:57 -0400

    Actualizar server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware para procesar JSON y servir archivos estáticos (frontend)
 app.use(express.json());
